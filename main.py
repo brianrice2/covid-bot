5e1d5ce7e6a1154468b06ad2be0d860915b483c0
Brian Rice <brianrice013@gmail.com>
2022-01-11 09:18:30 -0600

    Reformat printed output and refactor code

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pandas as pd
 
-url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
 
-if __name__ == '__main__':
-    # pull COVID data from NYT Github
+
+def main():
+    # Pull COVID data from NYT Github
     df = pd.read_csv(url)
 
-    # clean and engineer
-    df['date'] = pd.to_datetime(df['date'])
-    df['daily_cases'] = df['cases'] - df['cases'].shift(1, fill_value=0)
-    df['daily_deaths'] = df['deaths'] - df['deaths'].shift(1, fill_value=0)
+    # Clean and engineer features
+    df["date"] = pd.to_datetime(df["date"])
+    df["daily_cases"] = df["cases"] - df["cases"].shift(1, fill_value=0)
+    df["daily_deaths"] = df["deaths"] - df["deaths"].shift(1, fill_value=0)
 
-    # print daily cases and deaths
-    daily_stats = df.tail(1).loc[:, ['date', 'daily_cases', 'daily_deaths']].to_dict(orient='records')[0]
-    print(
-        daily_stats['date'].strftime('%B %d, %Y'), ': ',
-        daily_stats['daily_cases'], ' cases, ',
-        daily_stats['daily_deaths'], ' deaths',
-        sep=''
+    # Print daily cases and deaths
+    daily_stats = (
+        df.tail(1).loc[:, ["date", "daily_cases", "daily_deaths"]].to_dict(orient="records")[0]
     )
+    print(
+        f"""
+        {daily_stats["date"].strftime("%B %d, %Y")}:
+        {daily_stats["daily_cases"]:,} cases
+        {daily_stats["daily_deaths"]:,} deaths
+        """
+    )
+
+
+if __name__ == "__main__":
+    main()
